102aa1329e84af90b46ed6a5adf2cc48c988b43b
Ravi Dabhi <dabhiravi4763@gmail.com>
2023-11-27 17:52:10 +0530

    commit

--- todo_app/lib/App/Modules/Login/Login_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'Login_controller.dart';
-
-class LoginBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => LoginController());
-  }
-}
--- todo_app/lib/App/Routes/app_routes.dart
+++
@@ -1,4 +0,0 @@
-class AppRoutes {
-  static const login = "/login";
-  static const home = "/home";
-}
